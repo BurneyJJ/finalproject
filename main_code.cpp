e55fbbe1a0615e181aca921a7c27e0475fba6207
ceasidee <yuekeezhasuario25@gmail.com>
2026-06-15 23:27:19 +0800

    i have commitment issues, anyway please check this one. i did tweak sum doon sa  mga nasa taas then done na delete and upadate

--- main_code.cpp
+++ main_code.cpp
@@ -2,15 +2,20 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <cctype>
 using namespace std;
 
 const int MAX = 100;
 const string fName = "inventory.data";
 
+const int NUM_CATEGORIES = 10;
+const string CATEGORIES[NUM_CATEGORIES] = {
+    "Fresh Produce", "Meat & Poultry", "Seafood", "Beverage", "Candies & Chocolates", "Dairy", "Dry Goods", "Bakery", "Personal Care", "Cleaning Supplies";
+};
 struct Product{
     int id;
+    int categoryIndex
     string name;
-    string category;
     double price;
     int quantity;
 };
@@ -29,14 +34,17 @@ void addProduct();
 void updProduct();
 void delProduct();
 void searchProduct();
-void viewAll();
+void viewAll(); // lagyan ba to ng another void function to view by category?
+//void viewByCategory(int categoryIndex);
 void checkStock();
 void processOrder();
 void saveFile();
 void loadFile();
+int selectCategory();
 int  findProduct(int id);//kung gusto nila magsearch using ID (1001-1100)
-int  genId();//para sa int id, 1000 basis natin? like 1001-1100 para 100 items?
+int  genId(int  categoryIndex);//para sa int id, 1000 basis natin? like 1001-1100 para 100 items?
 bool confirm(const string& message);
+string getStockLevels(int quantity);
 
 
 int main() {
@@ -51,7 +59,7 @@ int main() {
         while (cin.fail()){
             cin.clear();
             cin.ignore(1000, '\n');
-            cout << "Invalid input. Enter a number: ";
+            cout << "Invalid input! Please Enter a number: ";
             cin >> choice;
         }
         cin.ignore();
@@ -66,10 +74,10 @@ int main() {
             case 7: processOrder(); break;
             case 0:
                 saveFile();
-                cout << "\nData saved. Goodbye.\n\n";
+                cout << "\nData saved. Goodbye!\n\n";
                 break;
             default:
-                cout << "\nInvalid choice. Try again.\n\n";
+                cout << "\nInvalid choice! Try again.\n\n";
         }
     } while(choice != 0);
     
@@ -79,7 +87,7 @@ int main() {
 void dispMenu(){
     cout << "\n========================================\n";
     cout << "     INVENTORY MANAGEMENT SYSTEM\n";
-    cout << "========================================\n";
+    cout << "==========================================\n";
     cout << "   [1] Add Product\n";
     cout << "   [2] Update Product\n";
     cout << "   [3] Delete Product\n";
@@ -93,31 +101,31 @@ void dispMenu(){
     cout << "========================================\n";
 }
 
+//FOR ADDING PRODUCTS
 void addProduct(){
-    //check if productCount >= MAX(100)
     if(productCount >= MAX){
-        cout << "\n[ERROR] Inventory is full! Cannot add more products.\n";
+        cout << "\n[ERROR!] Inventory is full! You cannot add more products.\n";
         return;
     }
     
-    //genId();
-    Product newProduct;
-    newProduct.id = genId();
-    
     cout << "\n--- ADD NEW PRODUCT ---\n";
-    cout << "Generated ID: " << newProduct.id << endl;
+    int categoryIndex = selectCategory();
     
-    //user input
-    //Category
-    cout << "Enter Category: ";
+    Product newProduct;
+    newProduct.categoryIndex = categoryIndex;
+    newProduct.id = genId(categoryIndex);
+    
+    cout << "Generated ID: " << newProduct.id << " (" << CATEGORIES[categoryIndex] << ") \n" << endl;
+    
+/* cout << "Enter Category: ";
     getline(cin, newProduct.category);
     while(newProduct.category.empty()){
         cout << "Category cannot be empty. Enter again: ";
         getline(cin, newProduct.category);
-    }
+    } gawin ko munang comment to kasi hindi na user defined ang categories*/
     
     //Name of Product
-    cout << "Enter Product Name: ";
+    cout << "Please Enter Product Name: ";
     getline(cin, newProduct.name);
     while(newProduct.name.empty()){
         cout << "Name cannot be empty. Enter again: ";
@@ -125,7 +133,7 @@ void addProduct(){
     }
     
     //Price
-    cout << "Enter Price (PHP): ";
+    cout << "Please Enter Price (PHP): ";
     cin >> newProduct.price;
     while(cin.fail() || newProduct.price < 0){
         cin.clear();
@@ -135,12 +143,12 @@ void addProduct(){
     }
     
     //Quantity(limit of 100 per product)
-    cout << "Enter Quantity (max 100): ";
+    cout << "Enter Quantity (MAXIMUM is 100): ";
     cin >> newProduct.quantity;
     while(cin.fail() || newProduct.quantity < 0 || newProduct.quantity > 100){
         cin.clear();
         cin.ignore(1000, '\n');
-        cout << "Invalid quantity. Enter 0-100: ";
+        cout << "Invalid quantity! Enter 0-100: ";
         cin >> newProduct.quantity;
     }
     
@@ -148,18 +156,148 @@ void addProduct(){
     inv[productCount] = newProduct;
     productCount++;
     
-    //display amount of product slots left
-    cout << "\n[SUCCESS] Product added successfully!\n";
-    cout << "Slots remaining: " << (MAX - productCount) << "/" << MAX << endl;
+    //intention = display updated list + remaining slots
+    cout << "\n[SUCCESS!] Your product has been added successfully!\n";
     
-    //saveFile();
+    cout << "- - - - - - - - - - - - - - -\n";
+    cout << "Here is your updated" << CATEGORIES[categoryIndex] << "list: \n";
+    viewByCategory(categoryIndex);
+    
+    cout << "- - - - - - - - - - - - - - -\n";
+    cout << "Slots remaining: " << (MAX - productCount) << "/" << MAX << endl;
+    cout << "- - - - - - - - - - - - - - -\n"; //natatawa akoo parang ang pangit nito
+
     saveFile();
 }
 
 void updProduct(){//enter product id, display product info, prompt user to add new info
+
+    /*MY INTENTION = enter id ex: 1001 = pork, 1002 = ground pork, automatic na dapat lalabas yung category dito*/
+    
+    if (productCount == 0){
+        cout << "\nNo Products Found in the Inventory\n";
+        return;
+    }
+    
+    int id;
+    cout << "\n--- UPDATE PRODUCT ---\n";
+    cout << "Please enter product ID: ";
+    cin >> id;
+    
+    while (cin.fail()){
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Invalid input! Please product ID: ";
+        cin >> id;
+    }
+    
+    int index = findProduct(id);
+    if (index == -1){
+        cout << "[ERROR!] Product not found. \n";
+        return;
+    }
+    
+    //pwede to na hindi nakaswitch kasi pwede na isa-isa sila magkakaroon ng condition
+    cout << "\nCurrent Product Information:\n";
+    cout << "  Product Name    :" << inv[index].name<< '\n';
+    cout << "  ID              :" << inv[index].id << '\n';
+    cout << "  Category        :" << CATEGORIES[inv[index].categoryIndex] << '\n';
+    //taas constant, itong baba pwede iupdate
+    cout << "  Price           : PHP " << inv[index].price << '\n'
+    cout << "  Quantity        :" << inv[index].quantity << '\n';
+    
+    //INTENTION = UPDATE PRICE OR QUANTITY
+    cout << "\nDo you want to make an update?\n";
+    cout << "  [1] Price \n";
+    cout << "  [2] Quantity\n";
+    cout << "  [0] Cancel/Exit\n";
+    cout << "Enter your Choice: ";
+    
+    int choice;
+    cin >> choice;
+    
+    while(cin.fail){
+        cin.clear();
+        cin.ignore(1000,\n);
+        cout << "Invalid Input! Please choose between 1, 2, and 0: ";
+        cin >> choice;
+    }
+    
+    switch (choice){
+        //pwede pa ito ayusin kasi baka ienter nila same price, hindi mag enter, basta ganon
+        case 1: 
+        cout << "Please enter a new price: PHP ";
+        cin >> inv[index].price;
+        cout << "Price has been updated successfully!\n";
+        break;
+        
+        case 2:
+        cout << "Please enter a new quantity: ";
+        cin >> inv[index].quantity;
+        cout << "Quantity has been updated successfully! \n";
+        break;
+        
+        case 0: 
+        cout << "Thank you! No changes were made. \n";
+        break;
+    }
+    
+    saveFile();
+    cout<< "\n[SUCESS!] Products updated successfully! \n";
+    
 }
 
 void delProduct(){//enter product id, confirm, delete
+    if (productCount == 0){
+        cout << "\nNo Products Found in the Inventory\n";
+        return;
+    }
+    
+    int id;
+    cout << "\n--- DELETE PRODUCT ---\n";
+    cout << "Please enter product ID: ";
+    cin >> id;
+    
+    while (cin.fail()){
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Invalid input! Please product ID: ";
+        cin >> id;
+    }
+    
+    int index = findProduct(id);
+    if (index == -1){
+        cout << "[ERROR!] Product not found.\n";
+        return;
+    }
+    
+    //tamad ako kaya copypasta na lang yung code na nauna
+    cout << "\nCurrent Product Information:\n";
+    cout << "  Product Name    :" << inv[index].name<< '\n';
+    cout << "  ID              :" << inv[index].id << '\n';
+    cout << "  Category        :" << CATEGORIES[inv[index].categoryIndex] << '\n';
+    cout << "  Price           : PHP " << inv[index].price << '\n'
+    cout << "  Quantity        :" << inv[index].quantity << '\n';
+    
+    //INTENTION = CONFIRMING THE DELETION
+    char confirm;
+    cout << "\nAre you sure you want to delete this product? : (y/n) \n";
+    cin >> confirm;
+    
+    if (confirm == 'y'|| confirm == 'Y'){
+        for (int i = index; i < productCount-1; i++){
+            inv[i] = inv[i+1];
+        }
+    
+    productCount--;
+    
+    saveFile();
+    cout << "[SUCESS!] Products has been updated successfully!";
+    } else {
+        cout << "Deletion cancelled. No updates has been made.";
+    }
+    cout << "Slots remaining: " << (MAX - productCount) << "/" << MAX << "\n";
+
 }
 
 void searchProduct(){//enter product id, display info if found  int id;
@@ -208,8 +346,6 @@ void viewAll(){//if productCount == 0, none found; otherwise, list all products 
         cout << endl;
     }
 }
-
-
 
 void checkStock(){//list product info and status (out of stock, low stock, in stock)
     if (productCount == 0){
